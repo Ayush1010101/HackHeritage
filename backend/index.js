fc9125654a16945bb2170029d236ab17b05d30c6
ArmaanTechDeveloper <kournarinder948@gmail.com>
2024-08-31 02:13:44 +0530

    connection to local database mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
-const express = require('express')
+import express from "express"
+import mongoose from "mongoose"
+import chalk from "chalk"
 
 const app = express();
 
 const PORT = 3000
 
-app.listen(PORT , () => {
-    console.log(`Listening on url http://localhost:${PORT}/`)
+mongoose.connection.once('open' , () => {
+    console.log(chalk.blue(`Mongo db connected successfully !!`))
 })
+
+const startServer = async () => {
+    await mongoose.connect('mongodb://localhost:27017/hackheritage')
+    app.listen(PORT , () => {
+        console.log(chalk.blue(`Listening on url http://localhost:${PORT}/`))
+    })
+}
+
+startServer()
