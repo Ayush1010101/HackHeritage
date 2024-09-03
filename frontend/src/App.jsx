df50f6d7ea0a4e587136eb911daa4942028c434b
Shreya <yshreya178@gmail.com>
2024-09-04 01:55:38 +0530

    main and app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import MainContent from './components/MainContent';
-
+import{Routes, Route } from 'react-router-dom';
 import './App.css'; // Import global styles or main CSS file
 
 const App = () => {
   return (
+    <>
     <div className="container">
       <NavBar />
       <MainContent />
     </div>
+     <Routes>
+     <Route path='/login' elements={<CureXLogin/>} />
+     <Route path='/signup' elements={<SignUp/>} />
+   </Routes>
+   </>
+
   );
 };
 
