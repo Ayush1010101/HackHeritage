97abff184b4223758265741b090c824ecdb7f3db
ArmaanTechDeveloper <kournarinder948@gmail.com>
2024-09-05 23:22:34 +0530

    POLLUTION: adding polluted code to run in hackathon

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,13 @@ import chalk from "chalk"
 import authRouter from "./auth/auth.router.js";
 import { checkAuth } from "./utils/token.js";
 import cookieParser from "cookie-parser";
+import path from "path"
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Get the filename and directory
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const app = express();
 
@@ -15,10 +22,34 @@ mongoose.connection.once('open' , () => {
 
 app.use(express.json())
 app.use(cookieParser())
-app.use('/auth' , authRouter)
+//app.use('/auth' , authRouter)
 
 app.get('/' , checkAuth , (req , res) => {
     res.send('Hello world')
+})
+
+// polluting start
+app.use(express.static(path.join(__dirname , '..')))
+app.get('/meds' , (req , res) => {
+    res.sendFile(path.join(__dirname , '..' , '/meds.html'))
+})
+app.get('/about' , (req , res) => {
+    res.sendFile(path.join(__dirname , '..' , '/about.html'))
+})
+app.get('/bb' , (req , res) => {
+    res.sendFile(path.join(__dirname , '..' , '/bb.html'))
+})
+app.get('/login' , (req , res) => {
+    res.sendFile(path.join(__dirname , '..' , '/login.html'))
+})
+app.get('/personal' , (req , res) => {
+    res.sendFile(path.join(__dirname , '..' , '/personal.html'))
+})
+app.get('/recommendation' , (req , res) => {
+    res.sendFile(path.join(__dirname , '..' , '/recommendation.html'))
+})
+app.get('/signup' , (req , res) => {
+    res.sendFile(path.join(__dirname , '..' , '/signup.html'))
 })
 
 const startServer = async () => {
